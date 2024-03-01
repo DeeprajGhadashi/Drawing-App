a436140e403ea170b52725fd3f1ffccb2c37de9f
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-03-02 00:39:24 +0530

    at time mouse click is not press then they do not wirte

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ window.addEventListener("load" , () => {
     canvas.height = canvas.offsetHeight;
 })
 
+const startDraw = () => {
+    isDrawing= true;
+}
+
 const drawing =(e) => {
     if(!isDrawing) return;  //if isDrawing is false return from here
     ctx.lineTo(e.offsetX, e.offsetY); // creating line according to the mouse pointer
     ctx.stroke(); //drawing/filing line with color
 }
 
+
+
+canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", ()=> isDrawing = false);
